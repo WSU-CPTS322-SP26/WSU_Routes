59530e7726e890bedddd4dfd8e5b50dc1a747df1
Hunter <hunter909@ymail.com>
2026-02-18 10:45:57 -0800

    added textbox

--- hunter_test/birdle/lib/main.dart
+++ hunter_test/birdle/lib/main.dart
@@ -14,13 +14,10 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Text('Birdle'),
-          ),
+          title: Align(alignment: Alignment.centerLeft, child: Text('Birdle')),
         ),
         body: Center(child: GamePage()),
-        ),
+      ),
     );
   }
 }
@@ -36,16 +33,77 @@ class GamePage extends StatelessWidget {
     // TODO: Replace with screen contents
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      
+
       child: Column(
         spacing: 5.0,
         children: [
-
-      ],)
+          for (var guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (var letter in guess) Tile(letter.char, letter.type),
+              ],
+            ),
+          GuessInput(
+            onSubmitGuess: (String guess) {
+              print(guess);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
 
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text.trim());
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+              onSubmitted: (_) {
+                _onSubmit();
+              },
+            ),
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: Icon(Icons.arrow_circle_up),
+          onPressed: () {
+             _onSubmit();
+          },
+        ),
+      ],
+    );
+  }
+}
 
 class Tile extends StatelessWidget {
   const Tile(this.letter, this.hitType, {super.key});
@@ -55,25 +113,24 @@ class Tile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return Container(
-      width:60,
-      height:60,
+      width: 60,
+      height: 60.0,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey.shade300),
         color: switch (hitType) {
           HitType.hit => Colors.green,
           HitType.partial => Colors.yellow,
           HitType.miss => Colors.grey,
-          _ => Colors.white, 
+          _ => Colors.white,
         },
       ),
       child: Center(
         child: Text(
           letter.toUpperCase(),
           style: Theme.of(context).textTheme.titleLarge,
-        )
-      )
+        ),
+      ),
     );
   }
 }
