c6c9b87f6663bd8074a0a12f71a4d1119b60b03c
Reed Williams <140353986+RJWilll@users.noreply.github.com>
2026-02-19 00:50:12 -0800

    Set up SQLAlchemy with temp database models
    Added SQLAlchemy configuration and defined models for Profile, Pin, Event, and Comment.

--- WSU_ROUTES_API/app.py
+++ WSU_ROUTES_API/app.py
@@ -11,9 +11,46 @@ app = Flask(__name__)
 app.debug = True
 CORS(app)
 
+database = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Disable tracking to save memory
+
+dataBase = SQLAlchemy(app)
+
+class Profile(dataBase.Model): #inital profile class
+    __tablename__ = 'profiles'
+    id = dataBase.Column(dataBase.String, primary_key=True, unique=True, nullable=False) #different from event and pin id
+    name = dataBase.Column(dataBase.String)
+    email = dataBase.Column(dataBase.String)
+    notifOn = dataBase.Column(dataBase.Bool)
+    isClub = dataBase.Column(dataBase.Bool)
+
+class Pin(dataBase.Model): #inital pin class
+    __tablename__ = 'pins'
+    id = dataBase.Column(dataBase.String, primary_key=True, unique=True, nullable=False) #different from event and profile id
+    name = dataBase.Column(dataBase.String)
+    isPublic = dataBase.Column(dataBase.Bool)
+    locationLat = dataBase.Column(dataBase.Float)
+    locationLong = dataBase.Column(dataBase.Float)
+
+class Event(dataBase.Model):
+    id = dataBase.Column(dataBase.String, primary_key=True, unique=True, nullable=False) #different from pin and profile id
+    name = dataBase.Column(dataBase.String)
+    pinId = dataBase.Column(dataBase.String) 
+
+class Comment(dataBase.Model):
+    pinId = dataBase.Column(dataBase.String, primary_key=True, nullable=False) #not unique because multiple comments can be on same pin
+    text = dataBase.Column(dataBase.String)
+    userId = dataBase.Column(dataBase.String)
+
+with app.app_context():
+    dataBase.drop_all() #can comment this out so the database isn't reset everytime, also database isn't in cloud rn, only on your system.
+    dataBase.create_all()
+
+
 #code run
 
-#some stuff will go here
+#supplemental code will go here
 
 #Queries
 
