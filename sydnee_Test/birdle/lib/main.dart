3227f1268492af33bd54b8b52a81d08e49a7b2cc
ravensramen <ravensramen@gmail.com>
2026-02-22 16:42:35 -0800

    Adding Reed's changes to my branch for testing

--- sydnee_Test/birdle/lib/main.dart
+++ sydnee_Test/birdle/lib/main.dart
@@ -10,13 +10,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          //child: Tile('A', HitType.hit)
-          //child: Tile('A', HitType.miss)
-          child: Tile('A', HitType.partial)
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+          ),
         ),
+        body: Center(child: GamePage()),
       ),
     );
   }
@@ -53,4 +55,31 @@ class Tile extends StatelessWidget {
     );
   }
 
+}
+
+class GamePage extends StatelessWidget{
+  GamePage({super.key});
+
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+    spacing: 5.0,
+    children: [
+      for (var guess in _game.guesses)
+        Row(
+          spacing: 5.0, 
+          children: [
+            for (var letter in guess)
+            Tile(letter.char, letter.type)
+          ]
+        )
+    ],
+    )
+    );
+  }
+  
 }
